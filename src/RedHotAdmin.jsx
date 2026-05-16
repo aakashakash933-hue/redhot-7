7a2a28aa2f37b181186c062fe2826fc8b35f2e24
awalikar886 <awalikar886@gmail.com>
2026-05-16 13:21:56 +0530

    update API URL to render

--- src/RedHotAdmin.jsx
+++ src/RedHotAdmin.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const API = "http://localhost:3001";
+const API = ("https://redhot-7.onrender.com/products");
 
 function useProducts() {
   const [products, setProducts] = useState([]);
